29735a153ec53481957a265b3996c60ed79cf397
Vitor George <vitor.george@gmail.com>
2021-01-22 13:36:12 +0000

    Add simple session workflow

--- app/assets/scripts/context/global.js
+++ app/assets/scripts/context/global.js
@@ -1,10 +1,16 @@
-import React, { createContext, useEffect, useReducer } from 'react';
+import React, { createContext, useEffect, useReducer, useState } from 'react';
 import T from 'prop-types';
-import { initialApiRequestState } from '../reducers/reduxeed';
+import { fetchJSON, initialApiRequestState } from '../reducers/reduxeed';
 import {
   createRestApiHealthReducer,
   queryRestApiHealth,
 } from '../reducers/api';
+import config from '../config';
+import request from 'request-promise';
+
+const session = request.jar();
+
+const { restApiEndoint } = config;
 
 const GlobalContext = createContext({});
 
@@ -14,15 +20,85 @@ export function GlobalContextProvider(props) {
     initialApiRequestState
   );
 
+  const [currentUser] = useState({
+    username: 'example',
+    email: 'example@example.com',
+    password: 'password123',
+  });
+
   useEffect(() => {
     queryRestApiHealth()(dispatchRestApiStatus);
   }, []);
+
+  useEffect(() => {
+    async function getWebsocketConnection() {
+      // try {
+      //   const res = await fetchJSON(`${restApiEndoint}/api/login`, {
+      //     method: 'POST',
+      //     headers: {
+      //       'Content-Type': 'application/json',
+      //     },
+      //     // credentials: 'include',
+      //     body: JSON.stringify({
+      //       username: currentUser.username,
+      //       password: currentUser.password,
+      //     }),
+      //   });
+      //   console.log(res);
+      // } catch (error) {
+      //   console.log(error.message);
+      // }
+
+      // try {
+      //   const res = await fetchJSON(`${restApiEndoint}/api/token`, {
+      //     method: 'POST',
+      //     credentials: 'include',
+      //     headers: {
+      //       'Content-Type': 'application/json',
+      //     },
+      //     body: JSON.stringify({
+      //       name: 'Access Token',
+      //     }),
+      //   });
+      //   console.log(res);
+      // } catch (error) {
+      //   console.log(error.message);
+      // }
+
+      await request({
+        method: 'POST',
+        json: true,
+        url: `${restApiEndoint}/api/login`,
+        jar: session,
+        body: {
+          username: currentUser.username,
+          password: currentUser.password,
+        },
+      });
+
+      await request({
+        method: 'POST',
+        json: true,
+        jar: session,
+        url: `${restApiEndoint}/api/token`,
+        body: {
+          name: 'Access Token',
+        },
+      });
+    }
+
+    const { isReady, hasError } = restApiHealth;
+    if (isReady() && !hasError()) {
+      getWebsocketConnection();
+    }
+  }, [restApiHealth, currentUser]);
 
   return (
     <>
       <GlobalContext.Provider
         value={{
           restApiHealth,
+          currentUser,
         }}
       >
         {props.children}
---
+++ app/assets/scripts/reducers/auth.js
@@ -0,0 +1,32 @@
+import {
+  makeAbortableActions,
+  makeFetchThunk,
+  makeAbortableAPIReducer,
+  wrapLogReducer,
+} from './reduxeed';
+import config from '../config';
+const { restApiEndoint } = config;
+
+/**
+ * USER SIGN UP
+ */
+const userSignUpActions = makeAbortableActions('USER_SIGN_UP');
+
+export function queryUserSignUp(payload) {
+  return makeFetchThunk({
+    url: `${restApiEndoint}/api/user`,
+    options: {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload),
+    },
+    requestFn: userSignUpActions.request,
+    receiveFn: userSignUpActions.receive,
+  });
+}
+
+export const createUserSignUpReducer = wrapLogReducer(
+  makeAbortableAPIReducer('USER_SIGN_UP')
+);
