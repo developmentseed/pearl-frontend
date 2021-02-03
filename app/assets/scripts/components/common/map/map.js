79c4c697226eaaf4630b5707569966da16105681
Jeevan Farias <jeevan@developmentseed.org>
2021-02-03 17:17:12 -0500

    Add comments

--- app/assets/scripts/components/common/map/map.js
+++ app/assets/scripts/components/common/map/map.js
@@ -9,21 +9,42 @@ const MapContainer = styled.div`
   z-index: 1;
 `;
 
+/* Componentized Leaflet Map container
+ * Map reference is passed as prop to children
+ * @param center - [lat, lon] map center
+ * @param zoom - [number] map zoom
+ */
+
 function MapComponent(props) {
-  const { children, center, zoom} = props;
+  const { children, center, zoom } = props;
   const [map, setMap] = useState();
   useEffect(() => {
-    let m = leaflet
-      .map('map', { zoomControl: false })
-      .setView(center || [0,0], zoom || 5);
-    leaflet
-      .tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      })
-      .addTo(m);
-    setMap(m);
+    let m = leaflet.map('map', { zoomControl: false });
+    m.on('load', () => {
+      setMap(m);
+      leaflet
+        .tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        })
+        .addTo(m);
+
+      if (process.env.NODE_ENV === 'development') {
+        // makes map accessible in console for debugging
+        window.map = m;
+      }
+    });
+    return () => {
+      map.remove();
+    };
+    /* eslint-disable-next-line react-hooks/exhaustive-deps */
   }, []);
+
+  useEffect(() => {
+    if (map) {
+      map.setView(center || [0, 0], zoom || 5);
+    }
+  }, [map, center, zoom]);
 
   return (
     <SizeAwareElement
@@ -47,10 +68,10 @@ function MapComponent(props) {
   );
 }
 
-MapComponent.propTypes = { 
+MapComponent.propTypes = {
   children: T.node,
   center: T.array,
-  zoom: T.number
+  zoom: T.number,
 };
 
 export default MapComponent;
