6deca99a8b81c8ae14beecab5995e5c63d6c610e
Vitor George <vitor.george@gmail.com>
2021-02-08 13:09:46 +0000

    Add Modal

--- app/assets/scripts/components/explore/prime-panel.js
+++ app/assets/scripts/components/explore/prime-panel.js
@@ -6,6 +6,10 @@ import { themeVal, glsp } from '@devseed-ui/theme-provider';
 import { Button } from '@devseed-ui/button';
 import { PanelBlock, PanelBlockHeader } from '../common/panel-block';
 import { Subheading } from '../../styles/type/heading';
+import SelectModal from './select-modal';
+import { Card } from './card-list';
+
+import { availableModels } from './sample-data';
 
 const SubheadingStrong = styled.strong`
   color: ${themeVal('color.base')};
@@ -43,66 +47,92 @@ export const EditButton = styled(Button).attrs({
 
 function PrimePanel() {
   const [selectedModel, setSelectedModel] = useState(null);
+  const [showSelectModelModal, setShowSelectModelModal] = useState(false);
   const [selectedArea, setSelectedArea] = useState(null);
 
   return (
-    <Panel
-      data-cy='primary-panel'
-      collapsible
-      direction='left'
-      initialState={true}
-      bodyContent={
-        <PanelBlock>
-          <PanelBlockHeader>
-            <HeadOptionHeadline>
-              <Subheading>Area: </Subheading>
-              <Subheading variation='primary'>
-                <SubheadingStrong>
-                  {selectedArea || 'Not selected'}
-                </SubheadingStrong>
-              </Subheading>
-              <EditButton
-                onClick={function () {
-                  setSelectedArea('1000 km2');
-                }}
-                title='Edit Model'
-                useIcon='area'
-              >
-                Clear and Edit Area
-              </EditButton>
-              <EditButton
-                onClick={function () {
-                  setSelectedArea(null);
-                }}
-                title='Edit Model'
-                useIcon='trash-bin'
-              >
-                Edit Area Selection
-              </EditButton>
-            </HeadOptionHeadline>
-          </PanelBlockHeader>
-          <PanelBlockHeader>
-            <HeadOptionHeadline>
-              <Subheading>Model: </Subheading>
-              <Subheading variation='primary'>
-                <SubheadingStrong>
-                  {selectedModel || 'Select Model'}
-                </SubheadingStrong>
-              </Subheading>
-              <EditButton
-                useIcon='swap-horizontal'
-                onClick={function () {
-                  setSelectedModel('CB Starter 123');
-                }}
-                title='Edit Model'
-              >
-                Edit Model Selection
-              </EditButton>
-            </HeadOptionHeadline>
-          </PanelBlockHeader>
-        </PanelBlock>
-      }
-    />
+    <>
+      <Panel
+        data-cy='primary-panel'
+        collapsible
+        direction='left'
+        initialState={true}
+        bodyContent={
+          <PanelBlock>
+            <PanelBlockHeader>
+              <HeadOptionHeadline>
+                <Subheading>Area: </Subheading>
+                <Subheading variation='primary'>
+                  <SubheadingStrong>
+                    {selectedArea || 'Not selected'}
+                  </SubheadingStrong>
+                </Subheading>
+                <EditButton
+                  onClick={function () {
+                    setSelectedArea('1000 km2');
+                  }}
+                  title='Edit Model'
+                  useIcon='area'
+                >
+                  Clear and Edit Area
+                </EditButton>
+                <EditButton
+                  onClick={function () {
+                    setSelectedArea(null);
+                  }}
+                  title='Edit Model'
+                  useIcon='trash-bin'
+                >
+                  Edit Area Selection
+                </EditButton>
+              </HeadOptionHeadline>
+            </PanelBlockHeader>
+            <PanelBlockHeader>
+              <HeadOptionHeadline>
+                <Subheading>Model: </Subheading>
+                <Subheading variation='primary'>
+                  <SubheadingStrong>
+                    {selectedModel || 'Select Model'}
+                  </SubheadingStrong>
+                </Subheading>
+                <EditButton
+                  data-cy='show-select-model-button'
+                  useIcon='swap-horizontal'
+                  onClick={function () {
+                    setShowSelectModelModal(true);
+                  }}
+                  title='Edit Model'
+                >
+                  Edit Model Selection
+                </EditButton>
+              </HeadOptionHeadline>
+            </PanelBlockHeader>
+          </PanelBlock>
+        }
+      />
+      <SelectModal
+        id='select-model-modal'
+        revealed={showSelectModelModal}
+        onOverlayClick={() => {
+          setShowSelectModelModal(false);
+        }}
+        data={availableModels}
+        renderCard={(model) => (
+          <Card
+            id={`model-${model.name}-card`}
+            key={model.name}
+            title={model.name}
+            size='large'
+            borderlessMedia
+            onClick={() => {
+              setShowSelectModelModal(false);
+              setSelectedModel(model.name);
+            }}
+          />
+        )}
+        nonScrolling
+      />
+    </>
   );
 }
 
---
+++ app/assets/scripts/components/explore/select-modal.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import T from 'prop-types';
+import styled from 'styled-components';
+import { Modal } from '@devseed-ui/modal';
+import CardList from './card-list';
+
+const BodyOuter = styled.div`
+  height: 45vh;
+`;
+
+const HeaderWrapper = styled(Modal)`
+  display: grid;
+  grid-template-columns: 1fr;
+`;
+
+const Headline = styled.h3`
+  text-align: center;
+`;
+
+export const ModalHeader = ({ id, title, children }) => {
+  return (
+    <HeaderWrapper id={id}>
+      <Headline>{title}</Headline>
+      {children}
+    </HeaderWrapper>
+  );
+};
+
+ModalHeader.propTypes = {
+  id: T.string,
+  title: T.string,
+  children: T.node,
+};
+
+function SelectModal(props) {
+  const {
+    id,
+    revealed,
+    onOverlayClick,
+    data,
+    renderHeader,
+    renderCard,
+    filterCard,
+    nonScrolling,
+  } = props;
+
+  return (
+    <Modal
+      data-cy={id}
+      id={id}
+      className='select'
+      size='xlarge'
+      revealed={revealed}
+      onOverlayClick={onOverlayClick}
+      closeButton={false}
+      renderHeader={renderHeader}
+      filterCard={filterCard}
+      content={
+        <BodyOuter>
+          <CardList
+            data={data}
+            renderCard={renderCard}
+            filterCard={filterCard}
+            nonScrolling={nonScrolling}
+          />
+        </BodyOuter>
+      }
+    />
+  );
+}
+
+SelectModal.propTypes = {
+  revealed: T.bool,
+  onOverlayClick: T.func,
+  data: T.array,
+  renderHeader: T.func,
+  renderCard: T.func,
+  filterCard: T.func,
+  nonScrolling: T.bool,
+};
+export default SelectModal;
