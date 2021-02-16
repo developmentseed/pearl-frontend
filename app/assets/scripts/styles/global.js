62c41e81a367e5fbadd7c7cc6909b8cd6b3c170d
Jeevan Farias <jeevan@developmentseed.org>
2021-02-16 12:38:20 -0500

    Remove devseed global style

--- app/assets/scripts/styles/global.js
+++ app/assets/scripts/styles/global.js
@@ -2,12 +2,9 @@ import { createGlobalStyle } from 'styled-components';
 import { collecticonsFont } from './collecticons';
 import leafletStyles from './vendor/leaflet';
 import reactToastStyles from './vendor/react-toastify';
-import devseedUiStyles from './vendor/devseed';
 
 export default createGlobalStyle`
   ${collecticonsFont()};
   ${leafletStyles()};
   ${reactToastStyles()};
-  ${devseedUiStyles()};
-
 `;
