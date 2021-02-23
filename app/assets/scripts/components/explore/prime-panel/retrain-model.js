f946393f68dbb8e877e0a7f8d3e6c9db6a2202e5
Vitor George <vitor.george@gmail.com>
2021-02-23 16:08:19 +0000

    Add buttons

--- app/assets/scripts/components/explore/prime-panel/retrain-model.js
+++ app/assets/scripts/components/explore/prime-panel/retrain-model.js
@@ -1,7 +1,21 @@
 import React from 'react';
-import { PlaceholderMessage } from '../../../styles/placeholder.js';
+import { Button } from '@devseed-ui/button';
+
 function RetrainModel() {
-  return <PlaceholderMessage>Retrain</PlaceholderMessage>;
+  return (
+    <>
+      <h3>Sample selection tools</h3>
+      <Button variation='base-raised-light' size='medium' useIcon='crosshair'>
+        Point
+      </Button>
+      <Button variation='base-raised-light' size='medium' useIcon='area'>
+        Draw
+      </Button>
+      <Button variation='base-raised-light' size='medium' useIcon='erase'>
+        Erase
+      </Button>
+    </>
+  );
 }
 
 RetrainModel.propTypes = {};
