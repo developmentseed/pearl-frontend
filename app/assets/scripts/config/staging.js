021003bdc34f2a875a3d0df638d01fb218f9422e
Sanjay B <sanjaybhangar@gmail.com>
2021-03-12 18:40:50 +0530

    refs #111, setup CI and config for production / staging deploys

--- app/assets/scripts/config/staging.js
+++ app/assets/scripts/config/staging.js
@@ -1,3 +1,9 @@
 export default {
   environment: 'staging',
+  restApiEndpoint: 'https://api.lulc-staging.ds.io',
+  audience: 'https://api.lulc-staging.ds.io',
+  NaipTileUrl:
+    'https://tiles.lulc-staging.ds.io/mosaic/naip.latest/tiles/{z}/{x}/{y}@1x?bidx=1%2C2%2C3',
+  tileUrlTemplate:
+    'https://tiles.lulc-staging.ds.io/mosaic/{LAYER_NAME}/tiles/{z}/{x}/{y}@1x?bidx=1%2C2%2C3',
 };
