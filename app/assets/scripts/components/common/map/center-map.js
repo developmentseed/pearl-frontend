749d7c423498761a4568949cc628a4f42cc6bfeb
Jeevan Farias <jeevan@developmentseed.org>
2021-03-12 11:38:25 -0500

    Lint

--- app/assets/scripts/components/common/map/center-map.js
+++ app/assets/scripts/components/common/map/center-map.js
@@ -1,9 +1,6 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useMap } from 'react-leaflet';
 import L from 'leaflet';
-import { Button } from '@devseed-ui/button';
-import collecticon from '@devseed-ui/collecticons';
-import { css } from 'styled-components';
 
 function CenterMap({ aoiRef }) {
   const map = useMap();
@@ -11,15 +8,16 @@ function CenterMap({ aoiRef }) {
     onAdd: function () {
       const container = L.DomUtil.create('div', 'leaflet-control  leaflet-bar');
 
-      const button = L.DomUtil.create('a', 'centerMap', container)
-      button.setAttribute('role',  'button')
-      button.setAttribute('href', '#')
-      button.setAttribute('title', 'Center Map')
-      button.onclick = () => map.fitBounds(aoiRef.getBounds())
+      const button = L.DomUtil.create('a', 'centerMap', container);
+      button.setAttribute('role', 'button');
+      button.setAttribute('href', '#');
+      button.setAttribute('title', 'Center Map');
+      button.onclick = () => map.fitBounds(aoiRef.getBounds());
 
       return container;
     },
-    onRemove: function (map) {},
+    // Don't need to do anything on remove
+    onRemove: () => {},
   });
   useEffect(() => {
     const center = new CenterControl({ position: 'topleft' });
