0caa18c19988b2d85498666a7a872d14b27776bc
Lane Goodman <Lane@developmentseed.org>
2021-03-17 14:50:54 -0400

    Maintain leaflet control color

--- app/assets/scripts/styles/vendor/leaflet.js
+++ app/assets/scripts/styles/vendor/leaflet.js
@@ -6,26 +6,30 @@ export default () => css`
     width: 30px;
   }
   .leaflet-control-geosearch form {
-    border-radius 4px;
+    border-radius: 4px;
   }
-  .leaflet-control-geosearch .reset{
+  .leaflet-control-geosearch .reset {
     display: none;
   }
 
-
-  .leaflet-control-geosearch a.leaflet-bar-part::before {
-    content:none;
+  .leaflet-bar a,
+  .leaflet-bar a:hover {
+    color: #121826;
+    opacity: 1;
   }
 
-  .leaflet-control-geosearch a.leaflet-bar-part.leaflet-bar-part-single::after{
+  .leaflet-control-geosearch a.leaflet-bar-part::before {
+    content: none;
+  }
+
+  .leaflet-control-geosearch a.leaflet-bar-part.leaflet-bar-part-single::after {
     ${collecticon('magnifier-left')};
     top: unset;
     left: unset;
-    height:100%;
+    height: 100%;
     width: 100%;
     border-radius: unset;
     border: unset;
-
   }
 
   .leaflet-control.leaflet-bar a.centerMap::after {
@@ -34,10 +38,10 @@ export default () => css`
 
   .leaflet-top.leaflet-left {
     .leaflet-control {
-      box-shadow: 0 -1px 1px 0 rgba(68,63,63,0.08), 0 2px 6px 0 rgba(68,63,63,0.16);
+      box-shadow: 0 -1px 1px 0 rgba(68, 63, 63, 0.08),
+        0 2px 6px 0 rgba(68, 63, 63, 0.16);
       border: none;
       border-radius: 0.25rem;
     }
   }
-
 `;
