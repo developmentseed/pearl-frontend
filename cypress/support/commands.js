b7db41ee31bcbd867ed7c3b0917141dde4facc8e
Vitor George <vitor.george@gmail.com>
2021-03-18 19:01:54 +0000

    Remove Auth0 dependency on cypress testing, stub requests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,32 +1,25 @@
 /**
- * Command login(username, password)
+ * Fake user login
  */
-Cypress.Commands.add('loginByAuth0Api', (username, password) => {
-  cy.log(`Logging in as ${username}`);
-  const client_id = Cypress.env('AUTH0_CLIENT_ID');
-  const audience = Cypress.env('AUTH0_AUDIENCE');
-  const scope = Cypress.env('AUTH0_SCOPE');
+Cypress.Commands.add('fakeLogin', () => {
+  window.localStorage.setItem(
+    'auth0Cypress',
+    JSON.stringify({
+      access_token: 'fake_access_token',
+      user: {
+        name: 'Test User',
+      },
+    })
+  );
+});
 
-  cy.request({
-    method: 'POST',
-    url: `https://${Cypress.env('AUTH0_DOMAIN')}/oauth/token`,
-    body: {
-      grant_type: 'password',
-      username,
-      password,
-      audience,
-      scope,
-      client_id,
-    },
-  }).then(({ body: { access_token, expires_in, id_token } }) => {
-    const auth0Cypress = {
-      user: JSON.parse(
-        Buffer.from(id_token.split('.')[1], 'base64').toString('ascii')
-      ),
-      access_token,
-      expires_in,
-    };
-
-    window.localStorage.setItem('auth0Cypress', JSON.stringify(auth0Cypress));
-  });
+/**
+ * Stub network requests
+ */
+Cypress.Commands.add('startServer', () => {
+  const apiEndpoint = (route) => `http://localhost:2000/${route}`;
+  cy.server();
+  cy.route('GET', apiEndpoint('health'), 'fixture:server/health.json');
+  cy.route('GET', apiEndpoint('api'), 'fixture:server/api.json');
+  cy.route('GET', apiEndpoint('api/mosaic'), 'fixture:server/api/mosaic.json');
 });
