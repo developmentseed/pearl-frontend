7c011c820523136420edae8faefcf6111430fd14
Jeevan Farias <jeevan@developmentseed.org>
2021-04-01 11:11:18 -0400

    Fix checkpoint header message

--- app/assets/scripts/components/explore/prime-panel/index.js
+++ app/assets/scripts/components/explore/prime-panel/index.js
@@ -203,6 +203,18 @@ function PrimePanel() {
       </SelectAoiTrigger>
     );
   };
+
+  const renderCheckpointSelectionHeader = () => {
+    if (currentCheckpoint && currentCheckpoint.id) {
+      return `${currentCheckpoint.name} (${currentCheckpoint.id})`;
+    } else if (checkpointList?.length) {
+      return `${checkpointList.length} checkpoint${
+        checkpointList.length > 1 ? 's' : ''
+      } available`;
+    } else {
+      return 'Run and retrain model to create checkpoints';
+    }
+  };
   // Retrain Panel Tab Empty State message
   //
   const retrainPlaceHolderMessage = () => {
@@ -337,9 +349,12 @@ function PrimePanel() {
                   <Subheading>Checkpoint</Subheading>
                 </HeadOptionHeadline>
                 <SubheadingStrong>
-                  {currentCheckpoint && currentCheckpoint.id
+                  {
+                    renderCheckpointSelectionHeader()
+                  }
+                  {/*currentCheckpoint && currentCheckpoint.id
                     ? `${currentCheckpoint.name} (${currentCheckpoint.id})`
-                    : 'Run and retrain model to create checkpoints'}
+                    : 'Run and retrain model to create checkpoints'*/}
                 </SubheadingStrong>
                 <HeadOptionToolbar>
                   <Dropdown
