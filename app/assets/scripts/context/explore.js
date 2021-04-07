df2fcd348085b97cb8023d328b83ad5fe0ba4ce4
Vitor George <vitor.george@gmail.com>
2021-04-07 09:33:00 +0100

    Move reverse geocode out of ExploreContext

--- app/assets/scripts/context/explore.js
+++ app/assets/scripts/context/explore.js
@@ -22,8 +22,7 @@ import predictionsReducer, {
 } from './reducers/predictions';
 import { mapStateReducer, mapModes, mapActionTypes } from './reducers/map';
 import tBbox from '@turf/bbox';
-import tBboxPolygon from '@turf/bbox-polygon';
-import tCentroid from '@turf/centroid';
+import reverseGeoCode from '../utils/reverse-geocode';
 
 import {
   actions as checkpointActions,
@@ -367,55 +366,6 @@ export function ExploreProvider(props) {
     }
   }
 
-  /*
-   * Reverse geocode using Bing
-   *
-   * @param bbox - should be turf bbox [minx, miny, maxX, maxY] or polygon feature
-   */
-  async function reverseGeoCode(bbox) {
-    /*
-    if (!aoiBounds) {
-      console.error('defined bounds before reverse geocoding')
-    }*/
-
-    let center;
-    if (Array.isArray(bbox)) {
-      // Need to create a polygon
-      center = tCentroid(tBboxPolygon(bbox));
-    } else {
-      // Assume a polygon feature is provided
-      center = tCentroid(bbox);
-    }
-
-    const [lon, lat] = center.geometry.coordinates;
-
-    const address = await fetch(
-      `${config.bingSearchUrl}/Locations/${lat},${lon}?radius=${config.reverseGeocodeRadius}&includeEntityTypes=address&key=${config.bingApiKey}`,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        mode: 'cors',
-      }
-    )
-      .then((res) => res.json())
-      .catch(() => {
-        toasts.error('Error querying address');
-        return null;
-      });
-
-    let name;
-    if (address && address.resourceSets[0].estimatedTotal) {
-      // Use first result if there are any
-      name = address.resourceSets[0].resources[0].address.locality;
-    } else {
-      toasts.warn('AOI not geocodable, generic name used');
-      name = 'Area';
-    }
-    // else leave name undefined, should be set by user
-    return name;
-  }
-
   useEffect(() => {
     if (!aoiRef) {
       setAoiArea(null);
@@ -516,8 +466,6 @@ export function ExploreProvider(props) {
 
         updateProjectName,
         updateCheckpointName,
-
-        reverseGeoCode,
       }}
     >
       {props.children}
