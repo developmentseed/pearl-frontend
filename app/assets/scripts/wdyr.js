cd16602c3c454bb2871ff03c15075b6d30106601
Vitor George <vitor.george@gmail.com>
2021-04-08 10:33:12 +0100

    Lint, update WDYR message

--- app/assets/scripts/wdyr.js
+++ app/assets/scripts/wdyr.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import logger from './utils/logger';
 
 if (process.env.NODE_ENV === 'development') {
+  logger(
+    '"Why did you render" module was loaded, which might slow down React in development mode.'
+  );
   const whyDidYouRender = require('@welldone-software/why-did-you-render');
   whyDidYouRender(React, {
     trackAllPureComponents: true,
   });
-  console.log('wdyr loaded');
 }
