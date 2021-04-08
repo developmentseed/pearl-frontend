474b82da5e0292ce4888dd2d4d75a4725eb9c8b0
Jeevan Farias <jeevan@developmentseed.org>
2021-04-08 12:13:59 -0400

    Add footer file

---
+++ app/assets/scripts/components/explore/prime-panel/footer.js
@@ -0,0 +1,183 @@
+import React from 'react';
+import T from 'prop-types';
+import styled from 'styled-components';
+import { glsp } from '@devseed-ui/theme-provider';
+
+import { Heading } from '@devseed-ui/typography';
+import { Button } from '@devseed-ui/button';
+import { Form, FormInput } from '@devseed-ui/form';
+import { Dropdown, DropdownBody } from '../../../styles/dropdown';
+import { LocalButton } from '../../../styles/local-button';
+
+import InfoButton from '../../common/info-button';
+import { PanelBlockFooter } from '../../common/panel-block';
+
+const PanelControls = styled(PanelBlockFooter)`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: ${glsp()};
+  padding-bottom: ${glsp(2)};
+`;
+const SaveCheckpoint = styled(DropdownBody)`
+  padding: ${glsp()};
+`;
+
+function Footer(props) {
+  const {
+    dispatchCurrentCheckpoint,
+    currentCheckpoint,
+    checkpointActions,
+    checkpointModes,
+
+    updateCheckpointName,
+    localCheckpointName,
+    setLocalCheckpointName,
+
+    mapRef,
+
+    allowInferenceRun,
+
+    applyTooltip,
+    runInference,
+    retrain,
+  } = props;
+  return (
+    <PanelControls>
+      <Button
+        variation='primary-raised-light'
+        size='medium'
+        useIcon='tick--small'
+        style={{
+          gridColumn: '1 / 2',
+        }}
+        id='reset-button-trigger'
+        onClick={() => {
+          dispatchCurrentCheckpoint({
+            type: checkpointActions.CLEAR_SAMPLES,
+          });
+          mapRef.polygonDraw.clearLayers();
+        }}
+      >
+        Clear
+      </Button>
+      <Button
+        variation='primary-raised-light'
+        size='medium'
+        useIcon='tick--small'
+        style={{
+          gridColumn: '2 / -1',
+        }}
+        id='undo-button-trigger'
+      >
+        Undo
+      </Button>
+
+      {currentCheckpoint &&
+      currentCheckpoint.mode === checkpointModes.REFINE ? (
+        <InfoButton
+          data-cy='save-refine'
+          variation='primary-raised-dark'
+          size='medium'
+          useIcon='tick--small'
+          style={{
+            gridColumn: '1 / -1',
+          }}
+          onClick={() => {}}
+          // visuallyDisabled={!allowInferenceRun}
+          // info={applyTooltip}
+          id='save-refine'
+        >
+          Save Refinements
+        </InfoButton>
+      ) : (
+        <InfoButton
+          data-cy={allowInferenceRun ? 'run-model-button' : 'disabled'}
+          variation='primary-raised-dark'
+          size='medium'
+          useIcon='tick--small'
+          style={{
+            gridColumn: '1 / -1',
+          }}
+          onClick={() => {
+            allowInferenceRun && !currentCheckpoint
+              ? runInference()
+              : retrain();
+          }}
+          visuallyDisabled={!allowInferenceRun}
+          info={applyTooltip}
+          id='apply-button-trigger'
+        >
+          {!currentCheckpoint ? 'Run Model' : 'Retrain'}
+        </InfoButton>
+      )}
+      <Dropdown
+        alignment='center'
+        direction='up'
+        triggerElement={(triggerProps) => (
+          <InfoButton
+            variation='primary-plain'
+            size='medium'
+            useIcon='save-disk'
+            useLocalButton
+            style={{
+              gridColumn: '1 / -1',
+            }}
+            id='rename-button-trigger'
+            {...triggerProps}
+            disabled={!currentCheckpoint}
+          >
+            Save Checkpoint
+          </InfoButton>
+        )}
+      >
+        <SaveCheckpoint>
+          <Heading useAlt>Checkpoint name:</Heading>
+          <Form
+            onSubmit={(evt) => {
+              evt.preventDefault();
+              const name = evt.target.elements.checkpointName.value;
+              updateCheckpointName(name);
+            }}
+          >
+            <FormInput
+              name='checkpointName'
+              placeholder='Set Checkpoint Name'
+              value={localCheckpointName}
+              onChange={(e) => setLocalCheckpointName(e.target.value)}
+              autoFocus
+            />
+            <LocalButton
+              type='submit'
+              // size='small'
+              variation='primary-plain'
+              useIcon='save-disk'
+              title='Rename checkpoint'
+              data-dropdown='click.close'
+            >
+              Save
+            </LocalButton>
+          </Form>
+        </SaveCheckpoint>
+      </Dropdown>
+    </PanelControls>
+  );
+}
+
+Footer.propTypes = {
+  dispatchCurrentCheckpoint: T.func,
+  currentCheckpoint: T.object,
+  checkpointActions: T.object,
+  checkpointModes: T.object,
+
+  updateCheckpointName: T.func,
+  localCheckpointName: T.string,
+  setLocalCheckpointName: T.func,
+
+  mapRef: T.object,
+
+  allowInferenceRun: T.bool,
+  applyTooltip: T.string,
+  runInference: T.func,
+  retrain: T.func,
+};
+export default Footer;
