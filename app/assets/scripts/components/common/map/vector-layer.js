d5fa8d1b2f522ded58f6211d53445627648d0a7b
Vitor George <vitor.george@gmail.com>
2021-04-12 13:28:36 +0100

    Add layer on mount, re-add on url change

--- app/assets/scripts/components/common/map/vector-layer.js
+++ app/assets/scripts/components/common/map/vector-layer.js
@@ -1,13 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useMap } from 'react-leaflet';
 import L from 'leaflet';
 //import {} from 'leaflet.vectorgrid/dist/Leaflet.VectorGrid.bundled.js';
 import {} from 'leaflet.vectorgrid';
+import usePrevious from '../../../utils/use-previous';
 
 function VectorLayer(props) {
   const { url, token, pane, opacity } = props;
   const map = useMap();
-  const [layer, setLayer] = useState();
+  const [layer, setLayer] = useState(null);
+
+  const previousUrl = usePrevious(url);
 
   useEffect(() => {
     const options = {
@@ -28,10 +31,44 @@ function VectorLayer(props) {
         },
       },
     };
+
     const layer = L.vectorGrid.protobuf(url, options);
     layer.on('add', (v) => setLayer(v.target));
     layer.addTo(map);
-  }, [url, token]);
+
+    return () => {
+      layer.remove();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!layer || previousUrl === url) return;
+
+    layer.remove();
+
+    const options = {
+      fetchOptions: {
+        headers: {
+          Authorization: token,
+          'Content-Type': 'application/json',
+        },
+      },
+      pane: pane || 'mapPane',
+      vectorTileLayerStyles: {
+        data: {
+          color: '#9bc2c4',
+          fill: true,
+          fillColor: '#9bc2c4',
+          fillOpacity: 1,
+          radius: 5,
+        },
+      },
+    };
+
+    const layer = L.vectorGrid.protobuf(url, options);
+    layer.on('add', (v) => setLayer(v.target));
+    layer.addTo(map);
+  }, [url, layer]);
 
   useEffect(() => {
     if (!layer) {
@@ -39,6 +76,7 @@ function VectorLayer(props) {
     }
     layer.setOpacity(opacity);
   }, [opacity, layer]);
+
   return null;
 }
 
