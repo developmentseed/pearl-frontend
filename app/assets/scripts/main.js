0ac7f296a529e90fb69b6e73b1ab7507c8d1a129
Vitor George <vitor.george@gmail.com>
2021-04-12 14:34:37 +0100

    Render home differently in production

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,8 +1,11 @@
+import './wdyr';
 import '@babel/polyfill';
 import React, { useEffect } from 'react';
+import config from './config';
 import { DevseedUiThemeProvider } from '@devseed-ui/theme-provider';
 
 import { render } from 'react-dom';
+import { withAuthenticationRequired } from '@auth0/auth0-react';
 import GlobalStyles from './styles/global';
 import ErrorBoundary from './fatal-error-boundary';
 import { Router, Route, Switch } from 'react-router-dom';
@@ -11,13 +14,31 @@ import history from './history';
 import theme from './styles/theme';
 
 import Landing from './components/landing';
+import Home from './components/home';
+import Explore from './components/explore';
+import AoiMap from './components/aoi-map';
+import About from './components/about';
 import UhOh from './components/uhoh';
+import Projects from './components/profile/projects';
+import Maps from './components/profile/maps';
 
 import { GlobalContextProvider } from './context/global';
 import { CollecticonsGlobalStyle } from '@devseed-ui/collecticons';
 import GlobalLoadingProvider from '@devseed-ui/global-loading';
 import { ToastContainerCustom } from './components/common/toasts';
 import { AuthProvider } from './context/auth';
+import { ApiMetaProvider } from './context/api-meta';
+
+const ProtectedRoute = (
+  { component, ...args } // eslint-disable-line react/prop-types
+) => (
+  <Route
+    component={
+      window.Cypress ? component : withAuthenticationRequired(component)
+    }
+    {...args}
+  />
+);
 
 // Root component.
 function Root() {
@@ -34,23 +55,39 @@ function Root() {
         <Router history={history}>
           <DevseedUiThemeProvider theme={theme.main}>
             <GlobalLoadingProvider />
-            <GlobalContextProvider>
-              <CollecticonsGlobalStyle />
-              <GlobalStyles />
-              <Switch>
-                <Route exact path='/' component={Landing} />
-                {/* <Route path='/project/:projectId' component={Explore} />
-                <ProtectedRoute exact path='/profile/maps' component={Maps} />
-                <ProtectedRoute
-                  exact
-                  path='/profile/projects'
-                  component={Projects}
-                />
-                <Route path='/about' component={About} /> */}
-                <Route path='*' component={UhOh} />
-              </Switch>
-              <ToastContainerCustom />
-            </GlobalContextProvider>
+            <ApiMetaProvider>
+              <GlobalContextProvider>
+                <CollecticonsGlobalStyle />
+                <GlobalStyles />
+                <Switch>
+                  {config.environment === 'production' ? (
+                    <Route exact path='/' component={Landing} />
+                  ) : (
+                    <>
+                      <Route exact path='/' component={Home} />
+                      <Route
+                        path='/project/:projectId/aoi/:aoiId/map'
+                        component={AoiMap}
+                      />
+                      <Route path='/project/:projectId' component={Explore} />
+                      <ProtectedRoute
+                        exact
+                        path='/profile/maps'
+                        component={Maps}
+                      />
+                      <ProtectedRoute
+                        exact
+                        path='/profile/projects'
+                        component={Projects}
+                      />
+                      <Route path='/about' component={About} />
+                    </>
+                  )}
+                  <Route path='*' component={UhOh} />
+                </Switch>
+                <ToastContainerCustom />
+              </GlobalContextProvider>
+            </ApiMetaProvider>
           </DevseedUiThemeProvider>
         </Router>
       </ErrorBoundary>
