e2424adfb5b81e8e864338819776b006b2222d2a
Lane Goodman <Lane@developmentseed.org>
2021-04-15 15:48:11 -0400

    Style pagination

--- app/assets/scripts/components/common/paginator.js
+++ app/assets/scripts/components/common/paginator.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
+import T from 'prop-types';
 import fill from 'fill-range';
 
-const PaginatorContainer = styled.div``;
+import { Button } from '@devseed-ui/button';
 
-const PageNumber = styled.span``;
-
-const PrevArrow = styled.div`
-  content: '<';
-  cursor: pointer;
+const PaginatorContainer = styled.section`
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  text-align: center;
+  font-size: 0.875rem;
 `;
 
-const NextArrow = styled.div`
-  content: '>';
-  cursor: pointer;
+const Pager = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  list-style: none;
+  list-style-type: none !important;
+  & > :not(:first-child) {
+    margin-left: 1rem;
+  }
+  ${Button} {
+    box-shadow: none;
+  }
+`;
+
+const PageButton = styled(Button)`
+  & ~ & {
+    margin-left: 0.5rem;
+  }
 `;
 
 /**
@@ -28,36 +45,63 @@ function Paginator({ numPages, currentPage, gotoPage }) {
   const hasNext = currentPage < numPages;
   return (
     <PaginatorContainer>
-      <PrevArrow
-        disabled={!hasPrev}
-        onClick={() => {
-          if (hasPrev) {
-            gotoPage(currentPage - 1);
-          }
-        }}
-      />
-      <NextArrow
-        disabled={!hasNext}
-        onClick={() => {
-          if (hasNext) {
-            gotoPage(currentPage + 1);
-          }
-        }}
-      />
-      {fill(1, numPages).map((pageNumber) => (
-        <PageNumber
-          key={pageNumber}
-          isCurrent={pageNumber === currentPage}
-          onClick={() => gotoPage(pageNumber)}
-        >
-          {pageNumber}
-        </PageNumber>
-      ))}
+      <Pager>
+        <li>
+          <Button
+            disabled={!hasPrev}
+            size='small'
+            variation='base-raised-semidark'
+            useIcon='chevron-left--small'
+            hideText
+            onClick={() => {
+              if (hasPrev) {
+                gotoPage(currentPage - 1);
+              }
+            }}
+          >
+            Previous
+          </Button>
+        </li>
+        <li>
+          {fill(1, numPages).map((pageNumber) => (
+            <PageButton
+              key={pageNumber}
+              isCurrent={pageNumber === currentPage}
+              variation={pageNumber === currentPage && 'primary-plain'}
+              onClick={() => gotoPage(pageNumber)}
+            >
+              {pageNumber}
+            </PageButton>
+          ))}
+        </li>
+        <li>
+          <Button
+            disabled={!hasNext}
+            size='small'
+            variation='base-raised-semidark'
+            useIcon='chevron-right--small'
+            hideText
+            onClick={() => {
+              if (hasNext) {
+                gotoPage(currentPage + 1);
+              }
+            }}
+          >
+            Next
+          </Button>
+        </li>
+      </Pager>
       <div>
         Showing {currentPage} of {numPages}
       </div>
     </PaginatorContainer>
   );
 }
+
+Paginator.propTypes = {
+  numPages: T.number,
+  currentPage: T.number,
+  gotoPage: T.func,
+};
 
 export default Paginator;
