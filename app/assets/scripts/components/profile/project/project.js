11e716dbe8d2fe82994d5aec48f44d031410efb7
Sanjay B <sanjaybhangar@gmail.com>
2021-04-16 20:52:51 +0530

    lintfix

--- app/assets/scripts/components/profile/project/project.js
+++ app/assets/scripts/components/profile/project/project.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import { Button } from '@devseed-ui/button';
