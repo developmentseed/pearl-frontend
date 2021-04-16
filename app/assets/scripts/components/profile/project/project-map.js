2bb3e4dc900b4b23d17dc478a3f876efec32a10f
Sajjad Anwar <sajjadkm@gmail.com>
2021-04-16 15:44:37 +0530

    add bing static map

--- app/assets/scripts/components/profile/project/project-map.js
+++ app/assets/scripts/components/profile/project/project-map.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import T from 'prop-types';
-
+import tBbox from '@turf/bbox';
+import config from '../../../config';
 function ProjectMap({ bounds }) {
+  let bbox = tBbox(bounds);
+  // lat,lng
+  bbox = [bbox[1], bbox[0], bbox[3], bbox[2]];
+  const src = `https://dev.virtualearth.net/REST/v1/Imagery/Map/AerialWithLabels?mapArea=${bbox.join(',')}&key=${config.bingApiKey}`
   return (
     <div>
-      <img src='https://via.placeholder.com/350' />
+      <img src={src} />
     </div>
   );
 }
