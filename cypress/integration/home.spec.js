fcae1b520c4606b618f820d94ca22cfc811be1be
Vitor George <vitor.george@gmail.com>
2021-07-20 08:23:10 +0100

    Upgrade cypress

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,11 +1,8 @@
 // / <reference types="Cypress" />
 describe('The Home Page', () => {
-  before(() => {
+  it('successfully loads', () => {
     cy.startServer();
     cy.visit('/');
-  });
-
-  it('successfully loads', () => {
     cy.get('body');
   });
 });
