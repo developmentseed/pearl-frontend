69331c8c25a1e1cf483bf068e42f9902936677f5
Vitor George <vitor.george@gmail.com>
2021-08-24 16:57:21 +0100

    Add batch spec

---
+++ cypress/integration/project/batch.spec.js
@@ -0,0 +1,25 @@
+describe('Retrain existing project', () => {
+  beforeEach(() => {
+    cy.startServer();
+  });
+
+  it('on new project, show different button large on large aoi', () => {
+    cy.fakeLogin();
+
+    cy.visit('/project/new');
+
+    cy.get('[data-cy=project-name-modal]').should('be.visible');
+    cy.get('[data-cy=modal-project-input]').clear().type('Project name');
+    cy.get('[data-cy=create-project-button]').click();
+
+    // Set AOI
+    cy.get('[data-cy=aoi-edit-button]').click();
+    cy.get('#map')
+      .trigger('mousedown', 150, 150)
+      .trigger('mousemove', 400, 400)
+      .trigger('mouseup');
+
+    cy.get('[data-cy=proceed-anyway-button]').should('exist').click();
+    cy.wait('@reverseGeocodeCity');
+  });
+});
