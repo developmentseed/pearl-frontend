bd95562927b4fdaa27d7af3fc36bc05554d67a28
Lane Goodman <Lane@developmentseed.org>
2021-09-17 23:33:22 -0400

    Remove unused Checkpoints modal from user dropdown

--- app/assets/scripts/components/common/user-dropdown.js
+++ app/assets/scripts/components/common/user-dropdown.js
@@ -1,9 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Button } from '@devseed-ui/button';
-import CardList, { Card } from './card-list';
-import { Modal as BaseModal } from '@devseed-ui/modal';
-import styled from 'styled-components';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import {
   Dropdown,
   DropdownHeader,
@@ -13,41 +10,9 @@ import {
   DropdownTrigger,
 } from '../../styles/dropdown';
 import { Heading } from '@devseed-ui/typography';
-import { glsp } from '@devseed-ui/theme-provider';
-import {
-  FormGroup,
-  FormGroupHeader,
-  FormGroupBody,
-  FormLabel,
-  FormInput,
-} from '@devseed-ui/form';
 import { useAuth0 } from '@auth0/auth0-react';
-
-import { availableCheckpoints } from '../explore/sample-data';
 import { filterComponentProps } from '../../styles/utils/general';
 import { useAuth } from '../../context/auth';
-
-const Modal = styled(BaseModal)`
-  .modal__contents {
-    padding: 1rem;
-  }
-`;
-
-const Headline = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  padding-bottom: ${glsp(1)};
-
-  h1 {
-    margin: 0;
-  }
-
-  ${Button} {
-    height: min-content;
-    align-self: center;
-  }
-`;
 
 // Please refer to filterComponentProps to understand why this is needed
 const propsToFilter = [
@@ -61,7 +26,6 @@ const propsToFilter = [
 const StyledLink = filterComponentProps(Link, propsToFilter);
 
 function UserDropdown() {
-  const location = useLocation();
   const { loginWithRedirect, logout } = useAuth0();
   const { isAuthenticated, user, authStateIsLoading } = useAuth();
 
@@ -69,9 +33,6 @@ function UserDropdown() {
     logout({
       returnTo: window.location.origin,
     });
-
-  const [showCheckpoints, setShowCheckpoints] = useState(false);
-  const [checkpointFilterString, setCheckpointFilterString] = useState('');
 
   if (authStateIsLoading) {
     return (
@@ -130,18 +91,6 @@ function UserDropdown() {
                   My Projects
                 </DropdownItem>
               </li>
-              {location &&
-                location.pathname &&
-                location.pathname.startsWith('/explore') && (
-                  <li>
-                    <DropdownItem
-                      useIcon='git-fork'
-                      onClick={() => setShowCheckpoints(true)}
-                    >
-                      My Checkpoints
-                    </DropdownItem>
-                  </li>
-                )}
               <li>
                 <DropdownItem
                   as={StyledLink}
@@ -168,67 +117,6 @@ function UserDropdown() {
           </>
         </Dropdown>
       )}
-      <Modal
-        id='checkpoints-modal'
-        revealed={showCheckpoints}
-        size='xlarge'
-        onOverlayClick={() => setShowCheckpoints(false)}
-        renderHeader={() => (
-          <>
-            <Headline>
-              {' '}
-              <Heading>Saved Checkpoints</Heading>
-              <Button
-                hideText
-                variation='base-plain'
-                size='small'
-                useIcon='xmark'
-                onClick={() => setShowCheckpoints(false)}
-              >
-                Close modal
-              </Button>
-            </Headline>
-            <FormGroup>
-              <FormGroupHeader>
-                <FormLabel htmlFor='checkpoint-filter'>
-                  Search Checkpoints
-                </FormLabel>
-              </FormGroupHeader>
-              <FormGroupBody>
-                <FormInput
-                  type='text'
-                  id='checkpoint-filter'
-                  name='checkpoint-filter'
-                  onChange={(e) => setCheckpointFilterString(e.target.value)}
-                  placeholder='Filter checkpoints'
-                />
-              </FormGroupBody>
-            </FormGroup>
-          </>
-        )}
-        renderBody={() => (
-          <CardList
-            numColumns={3}
-            style={{ height: '45vh' }}
-            data={availableCheckpoints}
-            filterCard={(card) => {
-              return card.name.includes(checkpointFilterString.toLowerCase());
-            }}
-            renderCard={(ckpt) => (
-              <Card
-                id={ckpt.id}
-                title={ckpt.name}
-                details={ckpt}
-                key={ckpt.id}
-                cardMedia={
-                  <img width='100%' src='https://place-hold.it/120x68/#dbdbd' />
-                }
-                expanded
-              />
-            )}
-          />
-        )}
-      />
     </>
   );
 }
