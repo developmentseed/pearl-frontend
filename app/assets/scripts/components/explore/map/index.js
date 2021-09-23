0b830d6e6400d17ae2071b9ee67120a465ff2f52
Jeevan Farias <jeevan@developmentseed.org>
2021-09-23 12:45:32 -0400

    Fix tests

--- app/assets/scripts/components/explore/map/index.js
+++ app/assets/scripts/components/explore/map/index.js
@@ -130,17 +130,27 @@ function Map() {
   // Manage changes in map mode
   useEffect(() => {
     // Check if map mode changed and disable previous controls
-    /*
+
     if (mapState && mapState.previousMode) {
       switch (mapState.previousMode) {
         case mapModes.ADD_SAMPLE_POLYGON:
-          if (!shortcutState.overrideBrowseMode) {
-            // In override browse mode, we are enterring browse mode temporarily so se should not disable the polygon draw
+          if (shortcutState.overrideBrowseMode) {
+            mapRef.polygonDraw?.pause();
+          } else if (
+            !mapState.overrideBrowseMode &&
+            mapState.mode !== mapModes.BROWSE_MODE
+          ) {
+            // Keyboard shortcut SPACE will enter override mode when held down
+            // On KEYUP, we will dispatch an update to return to previousMode, but overrideBrowseMode
+            // will be set to false before the mode update can be dispatched
             mapRef.polygonDraw.disable();
           }
           break;
+        case mapModes.ADD_SAMPLE_FREEHAND: {
+          mapRef.freehandDraw?.disable();
+        }
       }
-    }*/
+    }
 
     /**
      * The following block enables/disables map edit controls. Ideally we should use
@@ -151,71 +161,51 @@ function Map() {
       case mapModes.CREATE_AOI_MODE:
         mapRef.aoi.control.draw.enable();
         mapRef._container.style.cursor = 'crosshair';
-        mapRef.freehandDraw?.disable();
-        mapRef.polygonDraw?.disable();
         break;
       case mapModes.EDIT_AOI_MODE:
         mapRef.aoi.control.draw.disable();
         mapRef.aoi.control.edit.enable(aoiRef);
-        mapRef.freehandDraw?.disable();
-        mapRef.polygonDraw?.disable();
         break;
       case mapModes.BROWSE_MODE:
-        if (mapRef) {
-          mapRef.freehandDraw?.disable();
-
-          // When override browse mode, we should pause polygon draw so as to preserve an inprogress polygon
-          if (shortcutState.overrideBrowseMode) {
-            mapRef.polygonDraw?.pause();
+        if (mapRef && aoiRef) {
+          // Only disable if something has been drawn
+          mapRef.aoi.control.draw.disable();
+          if (mapRef.aoi.control.edit._shape) {
+            mapRef.aoi.control.edit.disable();
           }
-          if (aoiRef) {
-            // Only disable if something has been drawn
-            mapRef.aoi.control.draw.disable();
-            if (mapRef.aoi.control.edit._shape) {
-              mapRef.aoi.control.edit.disable();
-            }
-            if (
-              mapState.previousMode === mapModes.CREATE_AOI_MODE ||
-              mapState.previousMode === mapModes.EDIT_AOI_MODE
-            ) {
-              // On confirm, zoom to bounds
-              mapRef.fitBounds(aoiRef.getBounds(), { padding: BOUNDS_PADDING });
-              mapRef._container.style.cursor = 'grab';
-              setSessionStatusMode('set-aoi');
-            }
+          if (
+            mapState.previousMode === mapModes.CREATE_AOI_MODE ||
+            mapState.previousMode === mapModes.EDIT_AOI_MODE
+          ) {
+            // On confirm, zoom to bounds
+            mapRef.fitBounds(aoiRef.getBounds(), { padding: BOUNDS_PADDING });
+            mapRef._container.style.cursor = 'grab';
+            setSessionStatusMode('set-aoi');
           }
         }
         break;
       case mapModes.ADD_SAMPLE_POINT:
-        mapRef.freehandDraw?.disable();
-        mapRef.polygonDraw?.disable();
         mapRef._container.style.cursor = 'crosshair';
         break;
       case mapModes.ADD_SAMPLE_POLYGON:
-        if (currentCheckpoint && currentCheckpoint.activeItem) {
-          mapRef.freehandDraw?.disable();
+        if (currentCheckpoint?.activeItem) {
           mapRef._container.style.cursor = 'crosshair';
           mapRef.polygonDraw.enable(currentCheckpoint.activeItem);
         }
         break;
       case mapModes.ADD_SAMPLE_FREEHAND:
-        mapRef.polygonDraw?.disable();
-        if (currentCheckpoint && currentCheckpoint.activeItem) {
+        if (currentCheckpoint?.activeItem) {
           mapRef._container.style.cursor = 'crosshair';
           mapRef.freehandDraw.enableAdd(currentCheckpoint.activeItem);
         }
         break;
       case mapModes.DELETE_SAMPLES:
-        mapRef.polygonDraw?.disable();
-        mapRef.freehandDraw?.disable();
-        if (currentCheckpoint && currentCheckpoint.activeItem) {
+        if (currentCheckpoint?.activeItem) {
           mapRef._container.style.cursor = 'grab';
           mapRef.freehandDraw.enableSubtract(currentCheckpoint.activeItem);
         }
         break;
       default:
-        mapRef.freehandDraw?.disable();
-        mapRef.polygonDraw?.disable();
         mapRef._container.style.cursor = 'grab';
         break;
     }
