c4bf4f6ae6e911dfcc170fb778f23dc7266d24be
Lane Goodman <Lane@developmentseed.org>
2021-10-22 15:51:50 -0400

    Update tour steps with images

--- app/assets/scripts/components/explore/tour.js
+++ app/assets/scripts/components/explore/tour.js
@@ -1,10 +1,10 @@
 export const tourSteps = [
   {
-    title: 'Welcome to Pearl',
+    title: 'Welcome to PEARL',
     target: '#welcome-trigger',
     content:
       'PEARL allows you to make a landcover map in minutes. Select an area of interest, choose a machine learning model, and click "Run" to get predictions. You can then retrain models to suit your needs. Click "Show me more" to view guidance, or "Skip" if you want to skip the introduction and jump right in.',
-    media: '/assets/graphics/content/guide-aoi.png',
+    media: '/assets/graphics/content/guide-analysis.png',
     disableBeacon: true,
     placement: 'center',
     spotlightClicks: true,
@@ -14,14 +14,16 @@ export const tourSteps = [
     target: '#select-model-trigger',
     content:
       'Land cover segmentation models are recommended for you based on your selected region. To see details about the model selected or to choose a different model, click the model swap icon to view available model information.',
+    media: '/assets/graphics/content/guide-models.png',
     disableBeacon: true,
     placement: 'right',
     spotlightClicks: true,
   },
   {
-    title: 'Select area',
+    title: 'Select area of interest',
     target: '#edit-aoi-trigger',
     content: `Select an area of interest to run the model. Click the pencil icon to start drawing on the map, then click the ✔ to confirm. Areas must be under {LIVE_INFERENCE_MAX_AREA} km² to retrain models, and under {INFERENCE_MAX_AREA} km² total to run inference.`,
+    media: '/assets/graphics/content/guide-aoi.png',
     disableBeacon: true,
     placement: 'right',
     spotlightClicks: true,
@@ -29,16 +31,17 @@ export const tourSteps = [
   {
     title: 'Upload area',
     target: '#upload-aoi-modal-button',
-    content: `You can also upload an area in a .geojson file.`,
+    content: `You can also choose to upload an area in a .geojson file.`,
     disableBeacon: true,
     placement: 'right',
     spotlightClicks: true,
   },
   {
-    title: 'Apply Model',
+    title: 'Run Starter Model',
     target: '#apply-button-trigger',
     content:
       'Run the model over your selected area of interest to infer land cover classes.',
+    media: '/assets/graphics/content/guide-predictions.png',
     disableBeacon: true,
     placement: 'right-end',
     spotlightClicks: true,
@@ -48,6 +51,7 @@ export const tourSteps = [
     target: '#retrain-tab-trigger',
     content:
       'After running the model, use the retraining tools to update your model. Select points or draw polygons to identify pixels to reclassify with alternate classes.',
+    media: '/assets/graphics/content/guide-retrain.png',
     disableBeacon: true,
     placement: 'right',
     spotlightClicks: true,
@@ -57,6 +61,7 @@ export const tourSteps = [
     target: '#refine-tab-trigger',
     content:
       'After running inference, use the refinement tools to change classes directly on your map. Fill in specified areas with a single class, or select a previous model checkpoint to run inference in your drawn area with an alternate trained model checkpoint.',
+    media: '/assets/graphics/content/guide-refine.png',
     disableBeacon: true,
     placement: 'right',
     spotlightClicks: true,
@@ -71,10 +76,11 @@ export const tourSteps = [
     spotlightClicks: true,
   },
   {
-    title: 'Export',
+    title: 'Export Land Cover Map',
     target: '#export-options-trigger',
     content:
       'Export results as a .geotiff, or generate a URL to share with anyone on the web.',
+    media: '/assets/graphics/content/guide-export.png',
     disableBeacon: true,
     placement: 'right',
     spotlightClicks: true,
