efca909c6b8fc27f055a950a42610fd05daa3db2
Jeevan Farias <jeevan@developmentseed.org>
2021-12-15 09:08:32 -0600

    Warn user if aoi is out of bounds

--- app/assets/scripts/components/explore/prime-panel/footer.js
+++ app/assets/scripts/components/explore/prime-panel/footer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import T from 'prop-types';
 import styled, { css } from 'styled-components';
 import { glsp, disabled as disabledStyles } from '@devseed-ui/theme-provider';
@@ -17,6 +17,7 @@ import {
 import { useInstance } from '../../../context/instance';
 import { Subheading } from '../../../styles/type/heading';
 import { useAoi, useAoiName } from '../../../context/aoi';
+import useFetch from '../../../utils/use-fetch';
 import { useApiLimits } from '../../../context/global';
 import { useMapState, sessionModes } from '../../../context/explore';
 import { mapModes } from '../../../context/reducers/map';
@@ -30,6 +31,9 @@ import {
   showGlobalLoadingMessage,
 } from '../../common/global-loading';
 import toasts from '../../common/toasts';
+
+import booleanWithin from '@turf/boolean-within';
+import bboxPolygon from '@turf/bbox-polygon';
 
 const PanelControls = styled(PanelBlockFooter)`
   display: grid;
@@ -62,6 +66,7 @@ function PrimeButton({
   mapRef,
   setAoiBounds,
 }) {
+  const [aoiWithinBounds, setAoiWithinBounds] = useState();
   const { sessionStatus, setSessionStatusMode } = useSessionStatus();
   const {
     runPrediction,
@@ -70,10 +75,39 @@ function PrimeButton({
     retrain,
     refine,
   } = useInstance();
-  const { aoiArea, setActiveModal, aoiRef, setCurrentAoi } = useAoi();
+  const {
+    aoiArea,
+    setActiveModal,
+    aoiRef,
+    setCurrentAoi,
+    aoiBounds,
+  } = useAoi();
   const { updateAoiName } = useAoiName();
   const { apiLimits } = useApiLimits();
   const { mapState, setMapMode } = useMapState();
+
+  const mosaicMeta = useFetch('mosaic/naip.latest');
+
+  useEffect(() => {
+    if (!aoiBounds || !mosaicMeta.isReady) return;
+    const aoiBboxPolygon = bboxPolygon([
+      aoiBounds.getWest(),
+      aoiBounds.getSouth(),
+      aoiBounds.getEast(),
+      aoiBounds.getNorth(),
+    ]);
+
+    const mosaicBounds = bboxPolygon(mosaicMeta.data.bounds);
+
+    const isWithinMosaicBounds = booleanWithin(aoiBboxPolygon, mosaicBounds);
+
+    if (!isWithinMosaicBounds) {
+      toasts.error(
+        'AOI is not within mosaic boundary. Inference is not permitted'
+      );
+    }
+    setAoiWithinBounds(isWithinMosaicBounds);
+  }, [aoiBounds, mosaicMeta.isReady]);
 
   const applyAoi = () => {
     setMapMode(mapModes.BROWSE_MODE);
@@ -240,6 +274,10 @@ function PrimeButton({
   const { mode } = sessionStatus;
 
   const checkDisabledState = () => {
+    if (!aoiWithinBounds) {
+      // If the aoi is outside of the mosaic bounds, we cannot run any type of inference in any mode
+      return true;
+    }
     if (mode === sessionModes.PREDICTION_READY) {
       // Only one batch prediction permitted at a time
       if (runningBatch && isBatchArea) {
