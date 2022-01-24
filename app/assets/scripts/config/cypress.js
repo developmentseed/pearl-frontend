9f76166bb9384bfde0509f201e5e44a7acdc4ed5
Vitor George <vitor.george@gmail.com>
2022-01-24 17:43:10 +0000

    Update environment

--- app/assets/scripts/config/cypress.js
+++ app/assets/scripts/config/cypress.js
@@ -1,7 +1,7 @@
 // module exports is required to be able to load from gulpfile.
 module.exports = {
   default: {
-    environment: 'testing',
+    environment: 'cypress',
     restApiEndpoint: 'http://localhost:2000',
     websocketEndpoint: 'ws://localhost:1999',
     instanceCreationTimeout: 5000,
