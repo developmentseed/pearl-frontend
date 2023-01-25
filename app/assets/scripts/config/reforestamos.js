288a44af136d97c750af465f2fe075d6e9f98ca2
Vitor George <vitor.george@gmail.com>
2023-01-25 09:40:08 -0300

    Add reforestamos environment

---
+++ app/assets/scripts/config/reforestamos.js
@@ -0,0 +1,13 @@
+// module exports is required to be able to load from gulpfile.
+module.exports = {
+  default: {
+    environment: 'production',
+    restApiEndpoint: '',
+    websocketEndpoint: '',
+    auth0Domain: '',
+    auth0ClientId: '',
+    tileUrlTemplate:
+      'https://planetarycomputer.microsoft.com/api/data/v1/mosaic/tiles/87b72c66331e136e088004fba817e3e8/{z}/{x}/{y}?asset_bidx=image|1%2C2%2C3&assets=image&collection=naip',
+    appInsightsKey: '',
+  },
+};
