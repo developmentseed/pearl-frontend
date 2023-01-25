e94e496923899b4255e9b16c43dceffb9469501d
Sajjad Anwar <sajjadkm@gmail.com>
2023-01-25 18:33:43 +0530

    add env for reforestamos

--- app/assets/scripts/config/reforestamos.js
+++ app/assets/scripts/config/reforestamos.js
@@ -2,12 +2,11 @@
 module.exports = {
   default: {
     environment: 'production',
-    restApiEndpoint: '',
-    websocketEndpoint: '',
-    auth0Domain: '',
-    auth0ClientId: '',
+    restApiEndpoint: 'https://api.reforestamos-staging.ds.io',
+    websocketEndpoint: 'wss://socket.reforestamos-staging.ds.io',
+    auth0Domain: 'pearl-landcover.us.auth0.com',
+    auth0ClientId: 'mXokXsHo1eabFlWuJyQPpzErqOX3wZ87',
     tileUrlTemplate:
-      'https://planetarycomputer.microsoft.com/api/data/v1/mosaic/tiles/87b72c66331e136e088004fba817e3e8/{z}/{x}/{y}?asset_bidx=image|1%2C2%2C3&assets=image&collection=naip',
-    appInsightsKey: '',
+      'https://planetarycomputer.microsoft.com/api/data/v1/mosaic/tiles/87b72c66331e136e088004fba817e3e8/{z}/{x}/{y}?asset_bidx=image|1%2C2%2C3&assets=image&collection=naip'
   },
 };
