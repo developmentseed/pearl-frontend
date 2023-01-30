1a7031b9151e3e6f144c4951e333fad56700ca13
Sajjad Anwar <sajjadkm@gmail.com>
2023-01-30 14:34:09 +0530

    lint

--- app/assets/scripts/config/reforestamos.js
+++ app/assets/scripts/config/reforestamos.js
@@ -7,6 +7,6 @@ module.exports = {
     auth0Domain: 'pearl-landcover.us.auth0.com',
     auth0ClientId: 'mXokXsHo1eabFlWuJyQPpzErqOX3wZ87',
     tileUrlTemplate:
-      'https://planetarycomputer.microsoft.com/api/data/v1/mosaic/tiles/87b72c66331e136e088004fba817e3e8/{z}/{x}/{y}?asset_bidx=image|1%2C2%2C3&assets=image&collection=naip'
+      'https://planetarycomputer.microsoft.com/api/data/v1/mosaic/tiles/87b72c66331e136e088004fba817e3e8/{z}/{x}/{y}?asset_bidx=image|1%2C2%2C3&assets=image&collection=naip',
   },
 };
