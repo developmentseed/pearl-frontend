4c746537d41fc0f0b066d2cdfb9e13498d12683d
Vitor George <vitor.george@gmail.com>
2023-03-28 11:04:45 +0100

    Move actions and services to separate files

---
+++ app/assets/scripts/context/project-xstate/actions.js
@@ -0,0 +1,308 @@
+import get from 'lodash.get';
+import { assign } from 'xstate';
+import L from 'leaflet';
+import turfBboxPolygon from '@turf/bbox-polygon';
+import turfArea from '@turf/area';
+import toasts from '../../components/common/toasts';
+import { BOUNDS_PADDING } from '../../components/common/map/constants';
+
+export const actions = {
+  setInitialContext: assign((context, event) => {
+    const { projectId, isAuthenticated, apiClient } = event.data;
+    return {
+      project: { id: projectId },
+      isAuthenticated,
+      apiClient,
+      globalLoading: {
+        disabled: false,
+      },
+    };
+  }),
+  setInitialData: assign((context, event) => ({ ...event.data })),
+  setProjectName: assign((context, event) => {
+    const { projectName } = event.data;
+    return {
+      project: {
+        ...context.project,
+        name: projectName,
+      },
+    };
+  }),
+  setCurrentAoiName: assign((context, event) => {
+    const { aoiName } = event.data;
+    const { currentAoi } = context;
+    return {
+      currentAoi: {
+        ...currentAoi,
+        name: aoiName,
+      },
+    };
+  }),
+  setCurrentImagerySource: assign((context, event) => {
+    const { imagerySource } = event.data;
+    return {
+      currentImagerySource: imagerySource,
+      mosaicSelector: {
+        disabled: false,
+        message: 'Select a mosaic',
+      },
+    };
+  }),
+  setCurrentMosaic: assign((context, event) => {
+    const { mosaic } = event.data;
+    return {
+      currentMosaic: mosaic,
+    };
+  }),
+  setCurrentModel: assign((context, event) => ({
+    currentModel: event.data.model,
+  })),
+  setCurrentAoi: assign((context, event) => ({
+    currentAoi: event.data.aoi,
+  })),
+  setCurrentInstance: assign((context, event) => ({
+    currentInstance: event.data.instance,
+  })),
+  setCurrentInstanceStatus: assign((context, event) => ({
+    currentInstance: {
+      ...context.currentInstance,
+      ...event.data,
+    },
+  })),
+  setCurrentCheckpoint: assign((context, event) => ({
+    currentCheckpoint: event.data,
+  })),
+  setCurrentTimeframe: assign((context, event) => ({
+    currentTimeframe: event.data,
+  })),
+  updateCurrentPrediction: assign((context, { data }) => {
+    // Get bounds
+    let predictions = get(context, 'currentPrediction.predictions', []);
+
+    const [minX, minY, maxX, maxY] = data.bounds;
+
+    // Build prediction object
+    predictions = predictions.concat({
+      key: predictions.length + 1,
+      image: `data:image/png;base64,${data.image}`,
+      bounds: [
+        [minY, minX],
+        [maxY, maxX],
+      ],
+    });
+
+    return {
+      sessionStatusMessage: `Received image ${data.processed} of ${data.total}...`,
+      currentPrediction: {
+        ...context.currentPrediction,
+        processed: data.processed,
+        total: data.total,
+        predictions,
+      },
+    };
+  }),
+  clearCurrentPrediction: assign(() => ({
+    currentPrediction: null,
+    globalLoading: {
+      disabled: true,
+    },
+  })),
+  initializeMap: assign((context, event) => {
+    const { mapRef } = event.data;
+
+    const { currentAoi } = context;
+    let aoiGeojson;
+    let aoiShape;
+
+    // Add currentAoi to map, if it exists
+    if (currentAoi && currentAoi.bounds) {
+      const aoiGeojson = {
+        type: 'Feature',
+        properties: {},
+        geometry: currentAoi.bounds,
+      };
+      const aoiShape = L.geoJSON(aoiGeojson).addTo(mapRef);
+      mapRef.fitBounds(aoiShape.getBounds(), {
+        padding: BOUNDS_PADDING,
+      });
+    }
+
+    return {
+      mapRef,
+      currentAoi: currentAoi && {
+        ...currentAoi,
+        shape: aoiShape,
+        geojson: aoiGeojson,
+      },
+    };
+  }),
+  setProject: assign((context, event) => ({
+    project: event.data.project,
+  })),
+  initializeNewProject: assign(() => {
+    return {
+      sessionStatusMessage: 'Set Project Name',
+      imagerySourceSelector: {
+        disabled: true,
+        message: 'Define AOI first',
+      },
+      mosaicSelector: {
+        disabled: true,
+        message: 'Define AOI first',
+      },
+      modelSelector: {
+        disabled: true,
+        placeholderLabel: 'Define AOI first',
+      },
+    };
+  }),
+  enableImagerySourceSelector: assign(() => {
+    return {
+      sessionStatusMessage: 'Select Mosaic & Model',
+      imagerySourceSelector: {
+        disabled: false,
+        message: 'Select Imagery Source',
+      },
+    };
+  }),
+  enableModelSelector: assign(() => ({
+    modelSelector: {
+      disabled: false,
+      placeholderLabel: 'Select Model',
+    },
+  })),
+  enablePredictionRun: assign(() => ({
+    sessionStatusMessage: 'Ready for prediction run',
+    primeButton: {
+      disabled: false,
+      label: 'Run Prediction',
+    },
+  })),
+  enterPredictionRun: assign(() => ({
+    sessionStatusMessage: 'Running prediction',
+    globalLoading: {
+      disabled: false,
+      message: 'Running prediction',
+    },
+    primeButton: {
+      disabled: false,
+      label: 'Run Prediction',
+    },
+  })),
+  resetMapEventHandlers: assign(() => {
+    return {
+      mapEventHandlers: {
+        dragging: true,
+      },
+    };
+  }),
+  setupNewRectangleAoiDraw: assign(() => {
+    return {
+      mapEventHandlers: {
+        dragging: false,
+        mousedown: true,
+        mouseup: false,
+        mousemove: false,
+      },
+    };
+  }),
+  startNewRectangleAoiDraw: assign((context, event) => {
+    return {
+      aoiStatusMessage: 'New AOI',
+      mapEventHandlers: {
+        dragging: false,
+        mousedown: false,
+        mouseup: true,
+        mousemove: true,
+      },
+      rectangleAoi: {
+        bounds: [event.data.latLng],
+      },
+    };
+  }),
+  updateRectangleAoiDraw: assign((context, event) => {
+    const boundStart = context.rectangleAoi.bounds[0];
+    const boundEnd = event.data.latLng;
+    const bounds = [boundStart, boundEnd];
+
+    // Update map layer
+    let shape = context.rectangleAoi.shape;
+    if (!shape) {
+      shape = L.rectangle(bounds, {
+        interactive: false,
+      }).addTo(context.mapRef);
+    } else {
+      shape.setBounds(bounds);
+    }
+
+    // Calculate area
+    const aoiGeojson = turfBboxPolygon(bounds.flat());
+    return {
+      currentAoi: {
+        area: turfArea(aoiGeojson),
+        geojson: aoiGeojson,
+      },
+      mapEventHandlers: {
+        dragging: false,
+        mousedown: false,
+        mouseup: true,
+        mousemove: true,
+      },
+      rectangleAoi: {
+        bounds,
+        shape,
+      },
+    };
+  }),
+  endNewRectangleAoiDraw: assign((context) => {
+    // Take rectangle bounds and generate GeoJSON polygon feature
+    const aoiGeojson = turfBboxPolygon(context.rectangleAoi.bounds.flat());
+
+    return {
+      currentAoi: {
+        area: turfArea(aoiGeojson),
+        geojson: aoiGeojson,
+      },
+      mapEventHandlers: {
+        dragging: false,
+        mousedown: false,
+        mouseup: true,
+        mousemove: true,
+      },
+    };
+  }),
+  initializeAoiList: assign(() => {
+    return {
+      sessionStatusMessage: 'Set AOI',
+      aoiStatusMessage: 'Draw area on map or upload an AOI geometry',
+    };
+  }),
+  handleInstanceCreationError: assign(() => {
+    toasts.error(
+      'Could not start instance at the moment, please try again later.'
+    );
+    return {
+      globalLoading: {
+        disabled: true,
+      },
+    };
+  }),
+  enterRetrainIsReady: assign(() => ({
+    sessionStatusMessage: 'Ready for retrain run',
+    primeButton: {
+      disabled: false,
+      label: 'Retrain Model',
+    },
+  })),
+  disableGlobalLoading: assign(() => ({
+    globalLoading: {
+      disabled: true,
+    },
+  })),
+  displayAbortButton: assign((context) => ({
+    globalLoading: {
+      ...context.globalLoading,
+      abortButton: true,
+    },
+  })),
+};
---
+++ app/assets/scripts/context/project-xstate/services.js
@@ -0,0 +1,251 @@
+import { reverseGeocodeLatLng } from '../../utils/reverse-geocode';
+import turfCentroid from '@turf/centroid';
+import config from '../../config';
+import get from 'lodash.get';
+import { delay } from '../../utils/utils';
+import { WebsocketClient } from './websocket-client';
+import logger from '../../utils/logger';
+
+export const services = {
+  fetchInitialData: async (context) => {
+    const {
+      apiClient,
+      project: { id: projectId },
+    } = context;
+
+    // Initialize project and aois
+    let project;
+    let aoisList;
+    let timeframesList;
+    let checkpointList;
+    let currentAoi;
+    let currentTimeframe;
+    let currentImagerySource;
+    let currentMosaic;
+    let currentModel;
+
+    // Fetch lists
+    const { mosaics: mosaicsList } = await apiClient.get('mosaic');
+    const { imagery_sources: imagerySourcesList } = await apiClient.get(
+      'imagery'
+    );
+    const { models: modelsList } = await apiClient.get('model');
+
+    // If project is not new, fetch project data
+    if (projectId !== 'new') {
+      project = await apiClient.get(`project/${projectId}`);
+
+      currentModel = modelsList.find((model) => model.id === project.model_id);
+
+      // Fetch project aois
+      aoisList = (await apiClient.get(`project/${projectId}/aoi`)).aois;
+
+      // If there are aois, fetch the first one's timeframes
+      if (aoisList.length > 0) {
+        currentAoi = aoisList[0];
+
+        timeframesList = (
+          await apiClient.get(
+            `project/${projectId}/aoi/${currentAoi.id}/timeframe`
+          )
+        ).timeframes;
+
+        if (timeframesList.length > 0) {
+          currentTimeframe = timeframesList[0];
+          currentMosaic = mosaicsList.find(
+            (mosaic) => mosaic.id === currentTimeframe.mosaic
+          );
+          currentImagerySource = imagerySourcesList.find(
+            (imagerySource) =>
+              imagerySource.id === currentMosaic.imagery_source_id
+          );
+        }
+      }
+
+      // Get project's checkpoints
+      checkpointList = (await apiClient.get(`project/${projectId}/checkpoint`))
+        .checkpoints;
+    }
+
+    return {
+      mosaicsList,
+      imagerySourcesList,
+      modelsList,
+      project: project || { id: 'new' },
+      aoisList,
+      checkpointList,
+      timeframesList,
+      currentAoi,
+      currentTimeframe,
+      currentImagerySource,
+      currentMosaic,
+      currentModel,
+    };
+  },
+
+  geocodeAoi: async (context) => {
+    const centroid = turfCentroid(context.currentAoi.geojson);
+    const [lng, lat] = centroid.geometry.coordinates;
+    const aoiName = await reverseGeocodeLatLng(lng, lat);
+    return { aoiName };
+  },
+  createProject: async (context) => {
+    const {
+      apiClient,
+      project: { name: projectName },
+      currentModel,
+    } = context;
+
+    const project = await apiClient.post('project', {
+      name: projectName,
+      model_id: currentModel.id,
+    });
+
+    // Update page URL. For the context of this page, it is ok not to use
+    // react-router as we are not keeping track of the project ID in the
+    // URL at this point.
+    window.history.replaceState(null, projectName, `/project/${project.id}`);
+
+    return { project };
+  },
+  createAoi: async (context) => {
+    const { apiClient } = context;
+    const { name, geojson } = context.currentAoi;
+    const { id: projectId } = context.project;
+
+    const aoi = await apiClient.post(`/project/${projectId}/aoi`, {
+      name: name,
+      bounds: geojson,
+    });
+
+    return { aoi };
+  },
+  requestInstance: async (context) => {
+    const { apiClient } = context;
+    const { id: projectId } = context.project;
+
+    let instance;
+
+    // Fetch active instances for this project
+    const activeInstances = await apiClient.get(
+      `/project/${projectId}/instance/?status=active`
+    );
+
+    // Reuse existing instance if available
+    if (activeInstances.total > 0) {
+      const { id: instanceId } = activeInstances.instances[0];
+      instance = await apiClient.get(
+        `/project/${projectId}/instance/${instanceId}`
+      );
+    } else {
+      instance = await apiClient.post(`/project/${projectId}/instance`);
+    }
+
+    // Confirm instance has running status
+    let instanceStatus;
+    let creationDuration = 0;
+    while (
+      !instanceStatus ||
+      creationDuration < config.instanceCreationTimeout
+    ) {
+      // Get instance status
+      instanceStatus = await apiClient.get(
+        `project/${projectId}/instance/${instance.id}`
+      );
+      const instancePhase = get(instanceStatus, 'status.phase');
+
+      // Process status
+      if (instancePhase === 'Running') {
+        break;
+      } else if (instancePhase === 'Failed') {
+        throw new Error('Instance creation failed');
+      }
+
+      // Update timer
+      await delay(config.instanceCreationCheckInterval);
+      creationDuration += config.instanceCreationCheckInterval;
+
+      // Check timeout
+      if (creationDuration >= config.instanceCreationTimeout) {
+        throw new Error('Instance creation timeout');
+      }
+    }
+
+    return { instance };
+  },
+  runPrediction: (context) => (callback, onReceive) => {
+    const { token } = context.currentInstance;
+    const websocket = new WebsocketClient(token);
+
+    onReceive((event) => {
+      if (event.type === 'Abort run') {
+        websocket.sendMessage({
+          action: 'instance#terminate',
+        });
+        // Ideally we should thrown an error here to make the service
+        // execute the 'onError' event, but XState doesn't support errors
+        // thrown inside onReceive. A fix is planned for XState v5, more
+        // here: https://github.com/statelyai/xstate/issues/3279
+        callback({ type: 'Prediction run was aborted' });
+      }
+    });
+
+    websocket.addEventListener('message', (e) => {
+      const { message, data } = JSON.parse(e.data);
+
+      switch (message) {
+        case 'info#connected':
+          // After connection, send a message to the server to request
+          // model status
+          websocket.sendMessage({
+            action: 'model#status',
+          });
+          break;
+        case 'model#status':
+          // If not already running or aborting, request prediction
+          if (!data.processing && !data.is_aborting) {
+            websocket.sendMessage({
+              action: 'model#prediction',
+              data: {
+                aoi_id: context.currentAoi.id,
+                mosaic: context.currentMosaic.id,
+              },
+            });
+          }
+          break;
+        case 'model#checkpoint':
+          callback({
+            type: 'Received checkpoint',
+            data: { currentCheckpoint: data },
+          });
+          websocket.sendMessage({
+            action: 'model#status',
+          });
+          break;
+        case 'model#timeframe':
+          callback({
+            type: 'Received timeframe',
+            data,
+          });
+          break;
+        case 'model#prediction':
+          callback({
+            type: 'Received prediction progress',
+            data,
+          });
+          break;
+        case 'model#prediction#complete':
+          callback({
+            type: 'Prediction run was completed',
+            data,
+          });
+          break;
+
+        default:
+          logger('Unhandled websocket message', message, data);
+          break;
+      }
+    });
+    return () => websocket.close();
+  },
+};
