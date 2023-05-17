53d7a4856016ec96c9ea2b7c70ee2401fc7985d6
Vitor George <vitor.george@gmail.com>
2023-05-17 18:32:16 +0100

    Improve prime button state

--- app/assets/scripts/components/project/prime-panel/prime-button.js
+++ app/assets/scripts/components/project/prime-panel/prime-button.js
@@ -2,31 +2,45 @@ import React from 'react';
 import InfoButton from '../../common/info-button';
 import { ProjectMachineContext } from '../../../fsm/project';
 import selectors from '../../../fsm/project/selectors';
+import { SESSION_MODES } from '../../../fsm/project/constants';
 
 export function PrimeButton() {
   const actorRef = ProjectMachineContext.useActorRef();
+  const sessionMode = ProjectMachineContext.useSelector(selectors.sessionMode);
+  const isLargeAoi = ProjectMachineContext.useSelector(selectors.isLargeAoi);
   const isPredictionReady = ProjectMachineContext.useSelector(
     selectors.isPredictionReady
   );
-  const isLargeAoi = ProjectMachineContext.useSelector(selectors.isLargeAoi);
+  const isRetrainReady = ProjectMachineContext.useSelector(
+    selectors.isRetrainReady
+  );
 
-  const buttonLabel = isLargeAoi
-    ? 'Run Batch Prediction'
-    : 'Run Live Prediction';
+  let buttonLabel;
+  let buttonDisabled = false;
+  let buttonTooltip;
+  if (sessionMode === SESSION_MODES.PREDICT) {
+    buttonLabel = isLargeAoi ? 'Run Batch Prediction' : 'Run Live Prediction';
+    buttonDisabled = !isPredictionReady;
+  } else if (sessionMode === SESSION_MODES.RETRAIN) {
+    buttonLabel = 'Retrain Model';
+    buttonDisabled = !isRetrainReady;
+    buttonTooltip = !isRetrainReady && 'Select classes and samples to retrain';
+  }
 
   return (
     <InfoButton
+      id='apply-button-trigger'
       data-cy='prime-button'
-      data-disabled={!isPredictionReady}
+      data-disabled={buttonDisabled}
       variation='primary-raised-dark'
       size='medium'
       useIcon='tick--small'
       style={{
         gridColumn: '1 / -1',
       }}
-      onClick={() => isPredictionReady && actorRef.send('Prime button pressed')}
-      visuallyDisabled={!isPredictionReady}
-      id='apply-button-trigger'
+      onClick={() => !buttonDisabled && actorRef.send('Prime button pressed')}
+      visuallyDisabled={buttonDisabled}
+      info={buttonTooltip}
     >
       {buttonLabel}
     </InfoButton>
--- app/assets/scripts/fsm/project/selectors.js
+++ app/assets/scripts/fsm/project/selectors.js
@@ -8,6 +8,8 @@ export const selectors = {
     context: { currentImagerySource, currentMosaic, currentModel, currentAoi },
   }) =>
     !!currentAoi && !!currentImagerySource && !!currentMosaic && !!currentModel,
+  isRetrainReady: ({ context: { retrainClasses, retrainSamples } }) =>
+    retrainClasses?.length > 0 && retrainSamples?.length > 0,
   isLargeAoi: ({ context: { currentAoi, apiLimits } }) => {
     return currentAoi?.area > apiLimits?.live_inference;
   },
