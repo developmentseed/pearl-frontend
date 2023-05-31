23004b5b57c3f16d0d4c6ac4698792efb11e98b7
Vitor George <vitor.george@gmail.com>
2023-05-31 12:36:15 +0100

    Fix URLs

--- app/assets/scripts/config/reforestamos-prod.js
+++ app/assets/scripts/config/reforestamos-prod.js
@@ -2,8 +2,8 @@
 module.exports = {
   default: {
     environment: 'production',
-    restApiEndpoint: 'https://api.reforestamos.ds.io/api/',
-    websocketEndpoint: 'wss://socket.reforestamos-staging.ds.io',
+    restApiEndpoint: 'https://api.reforestamos.ds.io',
+    websocketEndpoint: 'wss://socket.reforestamos.ds.io',
     auth0Domain: 'pearl-landcover.us.auth0.com',
     auth0ClientId: 'mXokXsHo1eabFlWuJyQPpzErqOX3wZ87',
     tileUrlTemplate:
