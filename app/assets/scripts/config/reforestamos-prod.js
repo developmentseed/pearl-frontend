2680b57cfd3b94d7301dac27a9fa347712c8e6e8
Vitor George <vitor.george@gmail.com>
2023-05-31 12:13:56 +0100

    Add Reforestamos prod config

---
+++ app/assets/scripts/config/reforestamos-prod.js
@@ -0,0 +1,12 @@
+// module exports is required to be able to load from gulpfile.
+module.exports = {
+  default: {
+    environment: 'production',
+    restApiEndpoint: 'https://api.reforestamos.ds.io/api/',
+    websocketEndpoint: 'wss://socket.reforestamos-staging.ds.io',
+    auth0Domain: 'pearl-landcover.us.auth0.com',
+    auth0ClientId: 'mXokXsHo1eabFlWuJyQPpzErqOX3wZ87',
+    tileUrlTemplate:
+      'https://planetarycomputer.microsoft.com/api/data/v1/mosaic/tiles/87b72c66331e136e088004fba817e3e8/{z}/{x}/{y}?asset_bidx=image|1%2C2%2C3&assets=image&collection=naip',
+  },
+};
