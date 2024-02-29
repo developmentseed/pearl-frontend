e6d63d5d6b2b6f6b4da193fbbed25a0f17ab18e4
LanesGood <Lane@developmentseed.org>
2024-02-29 15:11:40 -0500

    Update batch table with size and mosaic range

--- app/assets/scripts/components/profile/project/batch-list.js
+++ app/assets/scripts/components/profile/project/batch-list.js
@@ -3,6 +3,7 @@ import T from 'prop-types';
 import styled from 'styled-components';
 import { Button } from '@devseed-ui/button';
 import { Heading } from '@devseed-ui/typography';
+import tArea from '@turf/area';
 
 import Table, { TableRow, TableCell } from '../../common/table';
 import Paginator from '../../common/paginator';
@@ -17,14 +18,17 @@ import { formatDateTime } from '../../../utils/format';
 import logger from '../../../utils/logger';
 import useFetch from '../../../utils/use-fetch';
 import { downloadGeotiff } from '../../../utils/share-link';
+import { formatThousands } from '../../../utils/format';
+import { composeMosaicName } from '../../compare-map';
 
 const TABLE_PAGE_SIZE = 5;
 const TABLE_HEADERS = [
   'Id',
   'AOI Name',
+  'AOI Size (KM2)',
+  'Started',
   'Mosaic',
   'Status',
-  'Started',
   'Download',
 ];
 
@@ -104,7 +108,10 @@ const BatchRow = ({ batch, projectId }) => {
     <TableRow key={id}>
       <TableCell>{id}</TableCell>
       <TableCell>{aoi?.name}</TableCell>
-      <TableCell>{mosaic?.name}</TableCell>
+      <TableCell>{formatThousands(tArea(aoi?.bounds) / 1e6)}</TableCell>
+      <TableCell>
+        {composeMosaicName(mosaic.mosaic_ts_start, mosaic.mosaic_ts_end)}
+      </TableCell>
       <TableCell>
         {status === 'Processing' ? (
           <>
